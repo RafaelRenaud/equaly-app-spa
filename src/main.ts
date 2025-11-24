415ecd9f93662e693d3fb71c2b7938c372f042ee
RafaelRenaud <renaudrafa@hotmail.com>
2025-11-24 20:06:36 -0300

    fix: Melhorias na tela de login e PWA

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { AppComponent } from "./app/app.component";
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    const splash = document.getElementById("custom-splash-screen");
+
+    if (splash) {
+      setTimeout(() => {
+        splash.classList.add("hidden");
+        setTimeout(() => splash.remove(), 300);
+      }, 200);
+    }
+  })
   .catch((err) => console.error(err));

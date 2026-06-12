e8b8db0b1388cddb71b748e268a77ae4ff97da22
RafaelRenaud <renaudrafa@hotmail.com>
2026-06-12 17:17:07 -0300

    conf: Ajuste de Direcionamento para novo backend

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  api: {
-    authentication: "https://api.equaly.app//authentication/v2",
-    administration: "https://equalyadmin.reddune-fdeeb971.westus2.azurecontainerapps.io/administration/v1",
-    core: "https://equalycore.reddune-fdeeb971.westus2.azurecontainerapps.io/operations/v1"
-  },
-};
